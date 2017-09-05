67ebdc91c5af4b574fecb8e6163b031b7d68182c
Martin Gaitan <mgaitan@onapsis.com>
2017-09-05 04:30:16 -0300

    fix wsgi

--- escrutiniociudadano/wsgi.py
+++ escrutiniociudadano/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carreros.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "escrutiniociudadano.settings")
 
 application = get_wsgi_application()
